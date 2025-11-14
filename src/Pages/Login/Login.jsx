7c25788b2b788772dab03d6579b1a5433ae0f527
Lucas Gehner <lmggxf@umsystem.edu>
2025-11-14 12:17:04 -0600

    login page mostly created, needs media queries and adjustment to image size and sign up tag.

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,20 @@
+import { Link } from 'react-router-dom';
 import './Login.css'
 import logo from "../../assets/S&T_review_logo_trans.png"
 
 function Login() {
     return (
         <div className='login'>
-            <img alt='' src={logo}/>
-            <input type='email' placeholder='Email'></input>
-            <input type='password' placeholder='Password'></input>
-            <button>Login</button>
+            <div className="login-logo">
+                <img alt='' src={logo}/>
+                <h1>New to S&T Review? <Link to={"/sign-up"} >Sign up</Link></h1>
+            </div>
+            <div className="input-fields">
+                <input type='email' placeholder='Email'></input>
+                <input type='password' placeholder='Password'></input>
+                <button>Login</button>
+            </div>
+            
         </div>
     )
 }
