600278bbfac73f695adb129eb5eee7095eb88206
Lucas Gehner <lmggxf@umsystem.edu>
2025-12-07 16:54:51 -0600

    loaded highest rated buildings on homepage

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -6,21 +6,55 @@ import './Home.css'
 function Home() {
     //api base url
     const api = import.meta.env.VITE_API_URL;
+    const [topRatedBuildings, setTopRatedBuildings] = useState([]);
 
+    useEffect(() => {
+        async function loadTopRated(){
+            try {
+                const result = await fetch(`${api}/top-buildings`);
+                const data = await result.json();
 
-    return(
+                if (data.success) {
+                    setTopRatedBuildings(data.data);
+                }
+            } catch (err) {
+                console.error("Error fetching top buildings:", err);
+            }
+        }
+        loadTopRated();
+    }, [])
+
+    return (
         <>
             <div className="title">
                 <h1>The highest rated buildings on campus:</h1>
             </div>
+
             <div className="review-container">
-                {/* ask server to query database to return 3 highest rated buildings */}
-                {Basicreviews.map((review, index) => {
-                    return(<div className="review">
-                            <Link to={'/building/:buildingID'}><img key={index} src={review.r_img} style={{cursor: 'pointer'}}/></Link>
-                            <p>Building name: {review.r_bldingName}</p>
-                            <p>Rating: {review.r_rating}</p>
-                        </div>)
+                {topRatedBuildings.map((b, index) => {
+                    // Build image URL
+                    const imageURL = `${api}/static/building_images/${b.image_name}`;
+
+                    // Encode building name for router (spaces → %20 or _ depending on your routing)
+                    const encodedName = encodeURIComponent(b.name);
+
+                    return (
+                        <Link to={`/buildings/${encodedName}`}>
+                            <div key={index} className="review">
+                                
+                                    <img
+                                        src={imageURL}
+                                        alt={b.name}
+                                        className="building-img"
+                                        style={{ cursor: "pointer" }}
+                                    />
+                                
+
+                                <p>Building name: {b.name}</p>
+                                <p>Rating: {b.rating.toFixed(1)}</p>
+                            </div>
+                        </Link>
+                    );
                 })}
             </div>
         </>
